efd8343613e0855c3a9b295015deec3afc72c10d
Ólafur Nielsen <69799161+olafurnielsen@users.noreply.github.com>
2023-09-11 10:12:14 +0000

    Disable automatic poetry install due to problems

--- .devcontainer/02onContentUpdate.sh
+++ .devcontainer/02onContentUpdate.sh
@@ -3,5 +3,5 @@ echo "Running 02onContentUpdate.sh"
 
 # Run poetry install within orbitcloud_graviton
 
-cd /workspaces/orbitcloud_graviton
-poetry install
+#cd /workspaces/orbitcloud_graviton
+#poetry install

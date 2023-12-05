8fe68c8f47c165561fb7acf6f0736675817d9ce5
Tarjei <tarjei@orbit.is>
2023-12-05 19:28:54 +0000

    feat: initial storageaccount, privateendpoint, privatednszonegroup #30 #19 #25

---
+++ components/orbitcloud_graviton/az_network/__init__.py
@@ -0,0 +1,4 @@
+from .az_private_endpoint import az_private_endpoint, PrivateEndpointConfig
+from .az_private_dns_zone_group import az_privatednszonegroup
+
+__all__ = ["az_private_endpoint", "PrivateEndpointConfig", "az_privatednszonegroup"]

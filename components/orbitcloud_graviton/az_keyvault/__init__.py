66b6aff495fcb1c48b4a80931d5197feb449071e
Ólafur Nielsen <69799161+olafurnielsen@users.noreply.github.com>
2024-01-31 16:41:05 +0000

    refactor(acr+keyvault): Adopts new config schema

--- components/orbitcloud_graviton/az_keyvault/__init__.py
+++ components/orbitcloud_graviton/az_keyvault/__init__.py
@@ -1,3 +1,3 @@
-from .keyvault import az_keyvault, az_keyvault_from_config
+from .keyvault import KeyVaultConfig, az_keyvault, az_keyvault_from_config
 
-__all__ = ["az_keyvault", "az_keyvault_from_config"]
+__all__ = ["az_keyvault", "az_keyvault_from_config", "KeyVaultConfig"]

915f2a5d6f56398223d6184a98bfe891d5fac061
Ólafur Nielsen <69799161+olafurnielsen@users.noreply.github.com>
2024-02-14 18:49:03 +0000

    feat(config): Adds support for getting nested values from dictionary stack outputs
    Now possible to do stack://<project>/<stack>/<output>.<deeply>.<nested> attribute where output is a dict export

--- components/orbitcloud_graviton/az_lib/types.py
+++ components/orbitcloud_graviton/az_lib/types.py
@@ -1,8 +1,10 @@
 import re
+from functools import reduce
 from typing import Annotated, Any, Union
 from uuid import UUID
 
 import pulumi
+from pulumi.runtime.sync_await import _sync_await
 from pulumi_azure_native import resources
 from pydantic import AfterValidator, GetCoreSchemaHandler, ValidationInfo
 from pydantic_core import core_schema
@@ -11,7 +13,7 @@ from pydantic_core import core_schema
 class AzureResourceId:
     def __init__(self, id: str) -> None:
         self.id: str = id
-        params = self._params()
+        params: dict[str, str] = self._params()
 
         self.subscription_id = UUID(params["subscriptionId"])
         self.resource_group_name: str | None = params.get("resource_group_name")
@@ -57,7 +59,7 @@ class AzureResourceId:
         cls, source_type: Any, handler: GetCoreSchemaHandler
     ) -> core_schema.CoreSchema:
         return core_schema.with_info_after_validator_function(
-            cls.validate, handler(str), field_name=handler.field_name
+            function=cls.validate, schema=handler(str), field_name=handler.field_name
         )
 
     def _params(self) -> dict[str, str]:
@@ -95,6 +97,14 @@ def parse_stack_reference(v: str) -> tuple[str, str]:
     return stack_ref, output_name
 
 
+def deep_get(obj: dict, path, default=None) -> Any | None:
+    return reduce(
+        lambda d, key: d.get(key, default) if isinstance(d, dict) else default,
+        path.split("."),
+        obj,
+    )
+
+
 def get_resource_id(
     v: Union[pulumi.Output[str], str],
 ) -> str | pulumi.Output[str]:
@@ -106,6 +116,24 @@ def get_resource_id(
 
     if v.startswith("stack://"):
         stack_ref, output_name = parse_stack_reference(v)
+
+        if "." in output_name:
+            stack = pulumi.StackReference(name=v, stack_name=stack_ref)
+            parent: str = output_name.split(".")[0]
+            path: str = ".".join(output_name.split(".")[1:])
+
+            # Workaround until Pulumi supports async methods in Python
+            # https://github.com/pulumi/pulumi/issues/12172#issuecomment-1691499199
+            output: Any | type[None] | None = _sync_await(
+                awaitable=stack.get_output_details(name=parent),
+            ).value
+
+            if output and isinstance(output, dict):
+                child: Any | None = deep_get(output, path=path)
+                if child:
+                    return str(child)
+                raise ValueError(f"Key path {output_name} not found in stack {stack_ref}")
+
         stack_output: pulumi.Output[str] = pulumi.StackReference(stack_ref).require_output(
             name=output_name
         )
@@ -130,6 +158,7 @@ def get_resource_name(
 
     if v.startswith("stack://"):
         stack_ref, output_name = parse_stack_reference(v)
+
         stack_output: pulumi.Output[str] = pulumi.StackReference(stack_ref).require_output(
             name=output_name
         )
@@ -142,11 +171,11 @@ def get_resource_name(
 
 
 AzureIdRef = Annotated[
-    Union[pulumi.Output[str], str],
+    Union[str, pulumi.Output[str]],
     AfterValidator(func=get_resource_id),
 ]
 
 AzureNameRef = Annotated[
-    Union[pulumi.Output[str], str],
+    Union[str, pulumi.Output[str]],
     AfterValidator(func=get_resource_name),
 ]

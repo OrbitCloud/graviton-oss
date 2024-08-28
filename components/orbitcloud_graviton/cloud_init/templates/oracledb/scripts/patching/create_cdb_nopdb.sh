a25b609c6e8c9966b6d1cfbd46f532ae0e6f2c3d
Ólafur Tryggvason <olitryggva@orbit.is>
2024-08-28 20:54:57 +0000

    Fixing  log_archive_start

--- components/orbitcloud_graviton/cloud_init/templates/oracledb/scripts/patching/create_cdb_nopdb.sh
+++ components/orbitcloud_graviton/cloud_init/templates/oracledb/scripts/patching/create_cdb_nopdb.sh
@@ -38,6 +38,7 @@ fi
 
 # Create the postcdb.sql script
 cat > /tmp/postcdb.sql <<EOF
+alter system set log_archive_start=true scope=spfile;
 alter system set db_create_online_log_dest_1="${DATA_DIR}-redo1/" scope=both;
 alter system set db_create_online_log_dest_2="${DATA_DIR}-redo2/" scope=both;
 
